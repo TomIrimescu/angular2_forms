41555c73400af7feae7a3604ad1ea950295fcc3a
TomIrimescu <tom@irimescu.com>
2016-09-06 17:26:57 -0400

    Template driven form added.

--- src/app/template-driven/template-driven.component.ts
+++ src/app/template-driven/template-driven.component.ts
@@ -1,9 +1,33 @@
 import { Component } from '@angular/core';
+import { NgForm } from "@angular/forms";
 
-@Component({
-  moduleId: module.id,
-  selector: 'template-driven',
-  templateUrl: 'template-driven.component.html'
-})
+
+@Component( {
+	moduleId: module.id,
+	selector: 'template-driven',
+	templateUrl: 'template-driven.component.html',
+	styles: [ `
+	.ng-invalid {
+		border: 1px solid red;
+	}
+` ]
+} )
 export class TemplateDrivenComponent {
+	user = {
+		username: 'Tom',
+		email: 'tom@default.com',
+		password: 'test',
+		gender: 'male'
+	};
+
+	genders = [
+		'male',
+		'female'
+	];
+
+	onSubmit ( form: NgForm ) {
+/*		console.log( this.user );*/ /*the 'user' variable will pre-populate the form fields*/
+/*		console.log( form );*/ /*this is an example of a non-grouped form result*/
+		console.log( form.value ); /*this is an example of a grouped form result*/
+	}
 }
