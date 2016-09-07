bf03704287c029dfa674fb1d9ebde82fcf869ac3
TomIrimescu <tom@irimescu.com>
2016-09-07 19:54:41 -0400

    Added barrel files, revert to ES5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { TemplateDrivenComponent } from "./template-driven/template-driven.component";
-import { DataDrivenComponent } from "./data-driven/data-driven.component";
+import { TemplateDrivenComponent } from "./template-driven";
+import { DataDrivenComponent } from "./data-driven";
 
 import { AppComponent } from "./app.component";
 
@@ -16,7 +16,7 @@ import { AppComponent } from "./app.component";
   imports: [
     BrowserModule,
     FormsModule, // For template-driven approach
-    ReactiveFormsModule, // For data-driven approach
+    ReactiveFormsModule // For data-driven approach
   ],
   bootstrap: [ AppComponent ]
 } )
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 
-import { environment } from './app/';
+//import { environment } from './app/';
 import { AppModule } from "./app/app.module";
 
-if (environment.production) {
+// if (environment.production) {
   enableProdMode();
-}
+// }
 
 platformBrowserDynamic().bootstrapModule( AppModule );
