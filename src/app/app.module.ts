08d0a77902cded61709a468f7498a9fbe9e7d7be
TomIrimescu <tom@irimescu.com>
2016-09-06 20:35:21 -0400

    Data driven form added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,30 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { TemplateDrivenComponent } from "./template-driven/template-driven.component";
- 
+import { DataDrivenComponent } from "./data-driven/data-driven.component";
+
 import { AppComponent } from "./app.component";
- 
-@NgModule({
+
+
+@NgModule( {
   declarations: [
     AppComponent,
-    TemplateDrivenComponent
+    TemplateDrivenComponent,
+    DataDrivenComponent
   ],
   imports: [
     BrowserModule,
     FormsModule, // For template-driven approach
     ReactiveFormsModule, // For data-driven approach
   ],
-  bootstrap: [AppComponent]
-})
-export class AppModule {}
+  bootstrap: [ AppComponent ]
+} )
+export class AppModule {
+}
+
+
+
+
+
+
+
--- src/app/template-driven/template-driven.component.ts
+++ src/app/template-driven/template-driven.component.ts
@@ -3,10 +3,10 @@ import { NgForm } from "@angular/forms";
 
 
 @Component( {
-	moduleId: module.id,
-	selector: 'template-driven',
-	templateUrl: 'template-driven.component.html',
-	styles: [ `
+  moduleId: module.id,
+  selector: 'template-driven',
+  templateUrl: 'template-driven.component.html',
+  styles: [ `
 	.ng-invalid, .invalid {
 		border: 1px solid red;
 	}
@@ -18,21 +18,24 @@ import { NgForm } from "@angular/forms";
 ` ]
 } )
 export class TemplateDrivenComponent {
-	user = {
-		username: 'Tom',
-		email: 'tom@default.com',
-		password: 'test',
-		gender: 'male'
-	};
-
-	genders = [
-		'male',
-		'female'
-	];
-
-	onSubmit ( form: NgForm ) {
-/*		console.log( this.user );*/ /*the 'user' variable will pre-populate the form fields*/
-/*		console.log( form );*/ /*this is an example of a non-grouped form result*/
-		console.log( form.value ); /*this is an example of a grouped form result*/
-	}
+  user = {
+    username: 'Tom',
+    email: 'tom@default.com',
+    password: 'test',
+    gender: 'male'
+  };
+  
+  genders = [
+    'male',
+    'female'
+  ];
+  
+  onSubmit ( form: NgForm ) {
+    /*		console.log( this.user );*/
+    /*the 'user' variable will pre-populate the form fields*/
+    /*		console.log( form );*/
+    /*this is an example of a non-grouped form result*/
+    console.log( form.value );
+    /*this is an example of a grouped form result*/
+  }
 }
