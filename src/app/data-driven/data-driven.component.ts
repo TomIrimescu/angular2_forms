5129ef1f137b57c1b4f384774d6b3326c87a6ddd
TomIrimescu <tom@irimescu.com>
2016-09-07 17:53:14 -0400

    Data driven form custom and async validators

--- src/app/data-driven/data-driven.component.ts
+++ src/app/data-driven/data-driven.component.ts
@@ -6,6 +6,7 @@ import {
     FormArray,
     FormBuilder
 } from "@angular/forms";
+import { Observable } from "rxjs";
 
 
 @Component({
@@ -14,7 +15,7 @@ import {
   templateUrl: 'data-driven.component.html',
   styles: [ `
 	.ng-invalid {
-		border: 1px solid red;
+		border: 1px solid hotpink;
 	}
 	
 	.invalid-msg {
@@ -48,24 +49,63 @@ export class DataDrivenComponent {
     // OPTION 2 for building forms with Form Builder
     this.myForm = formBuilder.group({
       'userData': formBuilder.group({
-        'username': ['', Validators.required],
+        'username': ['', [Validators.required, this.exampleValidator]],
         'email': ['', [ Validators.required, Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?") ]]
       }),
       'password': ['', Validators.required],
       'gender': ['male'],
       'hobbies': formBuilder.array([
-        ['Cooking', Validators.required]
+        ['Cooking', Validators.required, this.asyncExampleValidator]
       ])
     });
+
+    // react to value changes in your form state
+    // this.myForm.valueChanges.subscribe(
+    //     (data: any) => console.log(data)
+    // );
+
+    // react to status changes in your form state
+    this.myForm.statusChanges.subscribe(
+        (data: any) => console.log(data)
+    );
   }
 
   onAddHobby(){
-    (<FormArray>this.myForm.find('hobbies')).push(new FormControl('', Validators.required));
+    (<FormArray>this.myForm.find('hobbies')).push(new FormControl('', Validators.required, this.asyncExampleValidator));
   }
 
   onSubmit() {
     console.log(this.myForm);
   }
+
+  onReset(){
+    this.myForm.reset();
+  }
+
+  exampleValidator(control: FormControl): {[s: string]: boolean}{
+    // this is the case where the validation will fail
+    if (control.value === 'Example'){
+      return {example: true};
+    }
+    // this is the case where the validation has succeeded
+    return null;
+  }
+
+  asyncExampleValidator(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>(
+      (resolve, reject) => {
+        setTimeout(() => {
+          if (control.value === 'Example') {
+            resolve({ 'invalid': true });
+          } else{
+            resolve(null);
+          }
+        }, 1500);
+      }
+    );
+    return promise;
+  }
+
 }
 
 
